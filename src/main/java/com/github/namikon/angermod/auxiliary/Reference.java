382a8ddba5efef726e60dad533b7607e03f4cd88
Namikon <namikon@gmx.net>
2015-06-20 15:25:36 +0200

    New config-folder name

--- src/main/java/com/github/namikon/angermod/auxiliary/Reference.java
+++ src/main/java/com/github/namikon/angermod/auxiliary/Reference.java
@@ -8,6 +8,6 @@ public class Reference {
 	public static final String MODID = "angermod";
 	public static final String NAME = "AngerMod. Makes your Mobs angry!";
 	public static final String VERSION = "GRADLETOKEN_VERSION";
-	public static final String COLLECTIONNAME = "DreamCraft";
+	public static final String COLLECTIONNAME = "GTNewHorizons";
 }
 
