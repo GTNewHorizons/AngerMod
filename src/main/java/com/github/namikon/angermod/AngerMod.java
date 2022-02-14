ee4d0f20fd246c97b1745d553ffaaae5de9928bd
bombcar <github@bombcar.com>
2022-02-14 11:03:36 -0600

    update buildscript

--- src/main/java/com/github/namikon/angermod/AngerMod.java
+++ src/main/java/com/github/namikon/angermod/AngerMod.java
@@ -26,25 +26,25 @@ import eu.usrv.yamcore.persisteddata.PersistedDataBase;
  * @author Namikon
  *
  */
-@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION, 
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION,
 dependencies = 	"required-after:Forge@[10.13.4.1614,);required-after:Baubles@[1.0.1.10,);" +
-		"required-after:YAMCore@[0.3,);", certificateFingerprint = "1cca375192a26693475fb48268f350a462208dce")
+		"required-after:YAMCore@[0.3,);")
 public class AngerMod {
 	public static AngerModConfig _cfgManager = null;
 	public static LogHelper Logger = new LogHelper("AngerMod");
-	
+
 	public static PlayerSpawnProtection SpawnProtectionModule = null;
 	public static BlockBreakEvent BlockTakeAggroModule = null;
 	public static EatCookedAnimalsEvent EatCookedAnimalsModule = null;
 	public static KamikazeRevenge KamikazeRevengeModule = null;
-	
+
 	public static boolean ModInitSuccessful = true;
 	//private static IPersistedDataBase _mPersistedConfig = null;
-	
+
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
-		try 
+		try
 		{
 			_cfgManager = new AngerModConfig(event.getModConfigurationDirectory(), Reference.COLLECTIONNAME, Reference.MODID);
 			if (!_cfgManager.LoadConfig())
@@ -53,7 +53,7 @@ public class AngerMod {
 			_mPersistedConfig = new PersistedDataBase(event.getModConfigurationDirectory(), "AngerStorage.ser", Reference.COLLECTIONNAME);
 			if (!_mPersistedConfig.Load())
 				ModInitSuccessful = false;*/
-			
+
 		}
 	    catch (Exception e)
 	    {
@@ -61,7 +61,7 @@ public class AngerMod {
 	    	Logger.DumpStack(e);
 	    }
 	}
-	
+
 	@EventHandler
 	public void Init(FMLInitializationEvent event)
 	{
@@ -74,7 +74,7 @@ public class AngerMod {
 				MinecraftForge.EVENT_BUS.register(BlockTakeAggroModule);
 			}
 
-			
+
 			if (_cfgManager.PlayerSpawnProtection)
 			{
 				Logger.info("Spawn-Protection is enabled. Players will be protected until they attack");
@@ -82,32 +82,32 @@ public class AngerMod {
 				MinecraftForge.EVENT_BUS.register(SpawnProtectionModule);
 				FMLCommonHandler.instance().bus().register(SpawnProtectionModule);
 			}
-			
+
 			if (_cfgManager.FriendlyMobRevenge)
 			{
 				Logger.info("FriendlyMobRevenge is enabled. Be careful what you eat...");
 				EatCookedAnimalsModule = new EatCookedAnimalsEvent();
-				MinecraftForge.EVENT_BUS.register(EatCookedAnimalsModule);				
+				MinecraftForge.EVENT_BUS.register(EatCookedAnimalsModule);
 			}
-			
+
 			if (_cfgManager.KamikazeMobRevenge)
 			{
 				Logger.info("KamikazeMobRevenge is enabled. Have fun :P");
 				KamikazeRevengeModule = new KamikazeRevenge(_cfgManager);
-				MinecraftForge.EVENT_BUS.register(KamikazeRevengeModule);				
+				MinecraftForge.EVENT_BUS.register(KamikazeRevengeModule);
 			}
 		}
 		else
 			Logger.warn(String.format("%s will NOT do anything as there where errors due the preInit event. Check the logfile!", Reference.MODID));
 	}
-	
+
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event)
 	{
-		
+
 	}
-	
-	
+
+
 	/** Do some stuff once the server starts
 	 * @param pEvent
 	 */
